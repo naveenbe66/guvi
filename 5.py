7ffc502170af0a8b95eeb24f9fc4c79ae17ea790
naveenbe66 <47234505+naveenbe66@users.noreply.github.com>
2019-02-01 18:52:14 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -3,9 +3,8 @@ b=int(input())
 c=int(input())
 if (a>b) and (a>c):
    largest = a
-elif (b>a) and (b>c):
-   largest = b
-else:
-   largest = c
-
-print(largest)
+   elif (b>a) and (b>c):
+      largest = b
+      else:
+         largest = c
+         print(largest)
