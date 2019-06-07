ede73cbcc548506c7ade2c2dabb1282df32ad579
naveenbe66 <47234505+naveenbe66@users.noreply.github.com>
2019-06-07 20:27:58 +0530

    Update pro20.py

--- pro20.py
+++ pro20.py
@@ -2,7 +2,7 @@ a3=list(map(int,input().split()))
 b3=sorted(list(map(int,input().split())))[::-1]
 n2=0
 for i in range(len(b3)):
-  if(a2[1]>=b3[i]):
+  if(a3[1]>=b3[i]):
     n2+=int(a3[1]/b3[i])
     a3[1]%=b3[i]
 if(a3[1]==0):
