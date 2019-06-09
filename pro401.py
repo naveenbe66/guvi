14204dccaa19a50ad8452881b2dfe514cdf71198
naveenbe66 <47234505+naveenbe66@users.noreply.github.com>
2019-06-09 20:38:46 +0530

    Rename pro40.py to pro401.py

---
+++ pro401.py
@@ -0,0 +1,11 @@
+f1=input()
+d="dhoni"
+y=len(d)
+o=len(f1)
+if y==o:
+    if "d" in f1 and "h" in f1 and "o" in f1 and "n" in f1 and "i" in f1:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
