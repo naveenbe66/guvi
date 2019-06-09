1aab5ae4416e72fdf84b4ca44fea7ec412b5582d
naveenbe66 <47234505+naveenbe66@users.noreply.github.com>
2019-06-09 20:48:30 +0530

    Create pro44.py

---
+++ pro44.py
@@ -0,0 +1,4 @@
+n1,p,q,r=map(int,input().split())
+a=[int(i) for i in input().split()]
+c=[p*a[i]+q*a[j]+r*a[k] for i in range(len(a)) for j in range(len(a)) for k in range(len(a)) if i<=j<=k]
+print(max(c))
